5abf8fd1f82813891a2460a7738b26ae82cd5a18
Tatiana <amora4ka@gmail.com>
2022-05-05 02:37:01 +0300

    feat: add func to display keybord

---
+++ js/vacab.js
@@ -0,0 +1,467 @@
+const keyboardItemes = [
+    {
+        'eventCode' : 'Backquote',
+        'eventKeyEn' : "`",
+        'eventKeyRu' : '',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit1',
+        'eventKeyEn' : '1',
+        'eventKeyRu' : '1',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit2',
+        'eventKeyEn' : '2',
+        'eventKeyRu' : '2',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit3',
+        'eventKeyEn' : '3',
+        'eventKeyRu' : '3',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit4',
+        'eventKeyEn' : '4',
+        'eventKeyRu' : '4',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit5',
+        'eventKeyEn' : '5',
+        'eventKeyRu' : '5',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit6',
+        'eventKeyEn' : '6',
+        'eventKeyRu' : '6',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit7',
+        'eventKeyEn' : '7',
+        'eventKeyRu' : '7',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit8',
+        'eventKeyEn' : '8',
+        'eventKeyRu' : '8',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit9',
+        'eventKeyEn' : '9',
+        'eventKeyRu' : '9',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Digit0',
+        'eventKeyEn' : '0',
+        'eventKeyRu' : '0',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Minus',
+        'eventKeyEn' : '-',
+        'eventKeyRu' : '-',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Equal',
+        'eventKeyEn' : '=',
+        'eventKeyRu' : '=',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Backspace',
+        'eventKeyEn' : 'delete',
+        'eventKeyRu' : 'delete',
+        'row' : 0,  
+    },
+
+    {
+        'eventCode' : 'Tab',
+        'eventKeyEn' : 'tab',
+        'eventKeyRu' : 'tab',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyQ',
+        'eventKeyEn' : 'q',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyW',
+        'eventKeyEn' : 'w',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyE',
+        'eventKeyEn' : 'e',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyR',
+        'eventKeyEn' : 'r',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyT',
+        'eventKeyEn' : 't',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyY',
+        'eventKeyEn' : 'y',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyU',
+        'eventKeyEn' : 'u',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyI',
+        'eventKeyEn' : 'i',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyO',
+        'eventKeyEn' : 'o',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'KeyP',
+        'eventKeyEn' : 'p',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'BracketLeft',
+        'eventKeyEn' : '[',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'BracketRight',
+        'eventKeyEn' : ']',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'Backslash',
+        'eventKeyEn' : '\\',
+        'eventKeyRu' : '',
+        'row' : 1,  
+    },
+
+    {
+        'eventCode' : 'CapsLock',
+        'eventKeyEn' : 'caps lock',
+        'eventKeyRu' : 'caps lock',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyA',
+        'eventKeyEn' : 'a',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyS',
+        'eventKeyEn' : 's',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyD',
+        'eventKeyEn' : 'd',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyF',
+        'eventKeyEn' : 'f',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyG',
+        'eventKeyEn' : 'g',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyH',
+        'eventKeyEn' : 'h',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyJ',
+        'eventKeyEn' : 'j',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyK',
+        'eventKeyEn' : 'k',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'KeyL',
+        'eventKeyEn' : 'l',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'Semicolon',
+        'eventKeyEn' : ';',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'Quote',
+        'eventKeyEn' : '\'',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'Enter',
+        'eventKeyEn' : 'return',
+        'eventKeyRu' : '',
+        'row' : 2,  
+    },
+
+    {
+        'eventCode' : 'ShiftLeft',
+        'eventKeyEn' : 'shift',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'KeyZ',
+        'eventKeyEn' : 'z',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'KeyX',
+        'eventKeyEn' : 'x',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'KeyC',
+        'eventKeyEn' : 'c',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'KeyV',
+        'eventKeyEn' : 'v',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'KeyB',
+        'eventKeyEn' : 'b',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'KeyN',
+        'eventKeyEn' : 'n',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'KeyM',
+        'eventKeyEn' : 'm',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'Comma',
+        'eventKeyEn' : ',',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'Period',
+        'eventKeyEn' : '.',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'Slash',
+        'eventKeyEn' : '/',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : 'ShiftRight',
+        'eventKeyEn' : 'shift',
+        'eventKeyRu' : '',
+        'row' : 3,  
+    },
+
+    {
+        'eventCode' : '',
+        'eventKeyEn' : 'fn',
+        'eventKeyRu' : '',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'ControlLeft',
+        'eventKeyEn' : 'control',
+        'eventKeyRu' : 'control',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'AltLeft',
+        'eventKeyEn' : 'option',
+        'eventKeyRu' : 'option',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'MetaLeft',
+        'eventKeyEn' : 'command',
+        'eventKeyRu' : 'command',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'Space',
+        'eventKeyEn' : '',
+        'eventKeyRu' : '',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'MetaLeft',
+        'eventKeyEn' : 'command',
+        'eventKeyRu' : 'command',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'AltLeft',
+        'eventKeyEn' : 'option',
+        'eventKeyRu' : 'option',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'ArrowLeft',
+        'eventKeyEn' : 'ᐊ',
+        'eventKeyRu' : 'ᐊ',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'ArrowUp',
+        'eventKeyEn' : 'ᐃ',
+        'eventKeyRu' : 'ᐃ',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'ArrowDown',
+        'eventKeyEn' : 'ᐁ',
+        'eventKeyRu' : 'ᐁ',
+        'row' : 4,  
+    },
+
+    {
+        'eventCode' : 'ArrowRight',
+        'eventKeyEn' : 'ᐅ',
+        'eventKeyRu' : 'ᐅ',
+        'row' : 4,  
+    },
+
+
+];
+
+function createKeyboard() {
+    const container = "<div class='container'><textarea class='textarea' rows='15'></textarea><div class='keyboard'><div class='row'></div><div class='row'></div><div class='row'></div><div class='row'></div><div class='row'></div></div><div class='info'><p>Клавиатура создана в операционной системе macOS</p><p>Для переключения языка комбинация: control + пробел</p></div></div>";
+    document.querySelector('.body').innerHTML = container;
+    const rows = document.querySelectorAll('.row');
+    rows.forEach((row, ind) => {
+        const rowKeys = keyboardItemes.filter(item => {
+            return item.row === ind;
+        });
+        rowKeys.forEach(elem => {
+            rows[ind].innerHTML += `<button class="key"><span>${elem.eventKeyEn}</span></button>`;
+        })
+    })
+}
+
+window.addEventListener('load', createKeyboard);
